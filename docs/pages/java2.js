a5635fc28b9b5e514b0404ba3407dfd8288b5b52
arrowarchive <rgerzevske@gmail.com>
2022-11-15 22:00:21 -0500

    hopefully my Javascript is fixed

--- docs/pages/java2.js
+++ docs/pages/java2.js
@@ -2,37 +2,37 @@ function calendar () {
 
     var fieldset = document.getElementByTagName('input');
     for (var i = 0; i < fieldset.length; i++) {
-        fieldset[i].addEventListener('click', highlight, false);
+        fieldset[i].addEventListener('click', highlight(), false);
     }
 }
 function highlight() {
     var id = this.id;
     switch (id) {
         case "person": {
-            var chars = document.getElementsByClassName("person");
-            for (var i = 0; i < chars.length; i++) {
-                chars[i].style.backgroundColor = "yellow";
+            var person = document.getElementsByClassName("person");
+            for (var i = 0; i < person.length; i++) {
+                person[i].style.backgroundColor = "yellow";
             }
         };
         break;
-        case "locations": {
-            var chars = document.getElementsByClassName("location");
-            for (var i = 0; i < places.length; i++) {
-                chars[i].style.backgroundColor = "cyan";
+        case "location": {
+            var location = document.getElementsByClassName("location");
+            for (var i = 0; i < location.length; i++) {
+                location[i].style.backgroundColor = "cyan";
             }
         };
         break;
-        case "animals": {
-            var animals = document.getElementsByClassName("critter");
-            for (var i = 0; i < objects.length; i++) {
-                chars[i].style.backgroundColor = "green";
+        case "animal": {
+            var animal = document.getElementsByClassName("critter");
+            for (var i = 0; i < animal.length; i++) {
+                animal[i].style.backgroundColor = "green";
             }
         };
         break;
-        case "objects": {
-            var objects = document.getElementsByClassName("objects");
-            for (var i = 0; i < objects.length; i++) {
-                chars[i].style.backgroundColor = "pink";
+        case "object": {
+            var objects = document.getElementsByClassName("object");
+            for (var i = 0; i < object.length; i++) {
+                object[i].style.backgroundColor = "pink";
             }
         };
         break;
